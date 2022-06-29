2ef0a232f658a7da186d45783dc73cace9000f65
hermkan <hermann.kanga.dev@gmail.com>
2022-06-28 22:34:13 -0400

    completed input desktop

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,8 +1,9 @@
 const Search = () => {
   return (
-    <div className="container search-container mx-auto px-6 w-327">
-      <div className=" bg-white w-327 dark:bg-19202D h-20 rounded shadow relative z-10 -mt-8 mb-8 ">
-        <div className="w-327 h-20 flex justify-between px-6">
+    <div className="container search-container mx-auto px-6 w-327 tablet:w-689 desktop:w-1110">
+      {/* mobile */}
+      <div className=" bg-white w-327 h-20 tablet:w-689 desktop:w-1110  dark:bg-19202D  rounded shadow relative z-10 -mt-8 mb-8 ">
+        <div className="w-327 h-20 tablet:hidden flex justify-between px-6">
           <div className="flex order-2 basis-1/4">
             <div className="flex items-center pr-3 h-full cursor-pointer">
               <button>
@@ -33,6 +34,90 @@ const Search = () => {
             className="text-19202D dark:bg-19202D dark:text-white focus:outline-none font-normal h-full basis-3/4 pr-4 flex items-center  text-body order-1"
             placeholder="Filter by title…"
           />
+        </div>
+
+        {/* tablet/desktop */}
+        <div className="bg-white hidden h-full dark:bg-19202D tablet:flex tablet:justify-center">
+          <div className="flex justify-center h-full w-full px-4">
+            <div className="basis-1/3 relative">
+              <div className="absolute text-gray-600 dark:text-gray-400 flex items-center h-full cursor-pointer">
+                <svg width={23} height={23} xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M17.112 15.059h-1.088l-.377-.377a8.814 8.814 0 002.15-5.784A8.898 8.898 0 008.898 0 8.898 8.898 0 000 8.898a8.898 8.898 0 008.898 8.899c2.211 0 4.23-.808 5.784-2.143l.377.377v1.081l6.845 6.832 2.04-2.04-6.832-6.845zm-8.214 0A6.16 6.16 0 118.9 2.737a6.16 6.16 0 010 12.322z"
+                    fill="#5964E0"
+                    fillRule="nonzero"
+                  />
+                </svg>
+              </div>
+              <input
+                id="email1"
+                className="focus:outline-none bg-white font-normal w-full h-full flex items-center pl-12 text-body"
+                placeholder="Placeholder"
+              />
+            </div>
+
+            <div className="basis-1/3 relative border-l border-r dark:border-gray-700">
+              <div className="absolute text-gray-600 dark:text-gray-400 flex items-center px-4  h-full cursor-pointer">
+                <svg width="17" height="24" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M14.358 2.451A8.3 8.3 0 008.448 0a8.3 8.3 0 00-5.911 2.451c-2.922 2.925-3.285 8.427-.786 11.76l6.697 9.683 6.687-9.669c2.508-3.347 2.145-8.85-.777-11.774zm-5.833 8.894a3.057 3.057 0 01-3.051-3.054 3.057 3.057 0 013.05-3.055 3.057 3.057 0 013.052 3.055 3.057 3.057 0 01-3.051 3.054z"
+                    fill="#5964E0"
+                    fillRule="nonzero"
+                  />
+                </svg>
+              </div>
+              <input
+                id="email2"
+                className="focus:outline-none bg-white font-normal w-full h-full flex items-center pl-12 text-body"
+                placeholder="Placeholder"
+              />
+            </div>
+
+            <div className="basis-1/3 relative">
+              <div className="pl-3 desktop:pl-8 h-full flex items-center gap-3">
+                <div className="bg-white dark:bg-gray-800 border rounded-sm border-gray-400 dark:border-gray-700 w-5 h-5 flex flex-shrink-0 justify-center items-center relative">
+                  <input
+                    type="checkbox"
+                    className="checkbox opacity-0 absolute cursor-pointer w-full h-full"
+                  />
+                  <div className="check-icon hidden bg-violet text-white rounded-sm">
+                    <svg
+                      className="icon icon-tabler icon-tabler-check"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={20}
+                      height={20}
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" />
+                      <path d="M5 12l5 5l10 -10" />
+                    </svg>
+                  </div>
+                </div>
+                <p className="text-body w-20 flex text-very-dark-blue desktop:w-2/3 dark:text-white font-bold">
+                  <span>Full Time</span>
+                  <span className="hidden desktop:block desktop:ml-1">
+                    Only
+                  </span>
+                </p>
+                <div className="w-1/3 ml-10 flex justify-end">
+                  <button className="w-20 h-12 bg-5964E0 text-white rounded-md flex items-center justify-center">
+                    Search
+                  </button>
+                </div>
+              </div>
+
+              <style>
+                {`  .checkbox:checked + .check-icon {
+                            display: flex;
+                        }`}
+              </style>
+            </div>
+          </div>
         </div>
       </div>
     </div>
